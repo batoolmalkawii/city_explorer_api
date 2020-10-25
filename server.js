2d2cf7230154cf22255739602556945029516ae9
Batool Malkawi <batoolmalkawii@gmail.com>
2020-10-25 16:31:36 +0300

    fixed setup

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.get('/', (reqeust, response)=>{
 app.get('/location', (request, response)=>{
     const locationData = require('./data/location.json');
     const city = request.query.city;
-    let location;
-    locationData.forEach(locationData=>{
-        location = new Location(city, locationData);
-    });
+    let location = new Location(city, locationData);
     response.json(location);
 });
 
@@ -36,7 +33,7 @@ app.use('*', (request, resp)=>{
 // Constructor
 function Location(city, locationData){
     this.search_query=city;
-    this.formated_query=locationData.display_name;
+    this.formatted_query=locationData.display_name;
     this.latitude = locationData.lat;
     this.longitude = locationData.lon;
 }
